53b41ae3d98a53083145f3090ecb1a5746dbbd26
saddadnabbil <saddadnabbil@gmail.com>
2025-01-18 21:19:48 +0700

    feat: breeze

--- resources/views/size-chart.blade.php
+++ resources/views/size-chart.blade.php
@@ -1,6 +1,10 @@
 
-<x-layout>
-  <x-slot:title>{{ $title }}</x-slot:title>
+<x-app-layout>
+      <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __($title) }}
+        </h2>
+    </x-slot>
   <div class="bg-gray-100">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,18 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
 
-Route::get('/wkkwk', function () {
-    return view('welcome');
-});
+Route::get('/category/{id}', [ProductController::class, 'getCategory'])->name('category');
+
 Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::resource('products', ProductController::class);
+
+Route::get('/size-chart', function () {
+    return view('size-chart', ['nama' => 'Size Chart', 'title' => 'Size Chart']);
+})->name('size-chart');
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About Us']);
+})->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,35 +26,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/cart', [OrderController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add/{id}', [OrderController::class, 'addToCart'])->name('cart.add');
+    Route::post('/cart/update/{id}', [OrderController::class, 'update'])->name('cart.update');
+    Route::post('/cart/remove/{id}', [OrderController::class, 'removeFromCart'])->name('cart.remove');
+    Route::post('/cart/checkout', [OrderController::class, 'checkout'])->name('cart.checkout');
+
+    Route::get('/thankyou/{id}', [OrderController::class, 'thankyou'])->name('thankyou');
 });
 
-Route::get('/category/{id}', [ProductController::class, 'getCategory'])->name('category');
 
-Route::get('/', [ProductController::class, 'index'])->name('home');
-Route::resource('products', ProductController::class);
-
-Route::get('/cart', [OrderController::class, 'index'])->name('cart.index');
-Route::post('/cart/add/{id}', [OrderController::class, 'addToCart'])->name('cart.add');
-Route::post('/cart/update/{id}', [OrderController::class, 'update'])->name('cart.update');
-Route::post('/cart/remove/{id}', [OrderController::class, 'removeFromCart'])->name('cart.remove');
-Route::post('/cart/checkout', [OrderController::class, 'checkout'])->name('cart.checkout');
-
-Route::get('/thankyou/{id}', [OrderController::class, 'thankyou'])->name('thankyou');
-
-Route::get('/inbox',function () {
-    return view('inbox', ['nama' => 'Dhafin Fadhilah', 'title' => 'Size Chart']);
-});
-Route::get('/notification', function () {
-    return view('notification', ['title' => 'About Us']);
-});
-Route::get('/manpict', function () {
-    return view('manpict');
-})->name('manpict');
-Route::get('/womenpict', function () {
-    return view('womenpict');
-})->name('womenpict');
-Route::get('/kidspict', function () {
-    return view('kidspict');
-})->name('kidspict');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
